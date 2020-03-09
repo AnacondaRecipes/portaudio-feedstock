68228719976b3354a872b9cca2e549d76379fb53
Jonathan Helmus <jjhelmus@gmail.com>
2020-03-09 14:45:59 -0500

    local version

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,9 @@
-copy "%RECIPE_DIR%\build.sh" .
-set PREFIX=%PREFIX:\=/%
-set SRC_DIR=%SRC_DIR:\=/%
-set MSYSTEM=MINGW%ARCH%
-set MSYS2_PATH_TYPE=inherit
-set CHERE_INVOKING=1
-bash -lc "./build.sh"
-if errorlevel 1 exit 1
-
-exit 0
+cmake -G"%CMAKE_GENERATOR%" ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DTARGET_POSTFIX= ^
+      .
+cmake --build . --target ALL_BUILD --config Release
+copy /y Release\*.dll %LIBRARY_BIN%
+copy /y Release\*.lib %LIBRARY_LIB%
+copy /y include\*.h %LIBRARY_INC%
