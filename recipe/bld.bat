1ee05ab236fea1cd00417c279d9d584a2c280062
Serhii Kupriienko <skupriienko@anaconda.com>
2025-06-04 12:42:25 +0300

    Update recipe

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,13 @@
-cmake -G"%CMAKE_GENERATOR%" ^
-      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+cmake %CMAKE_ARGS% -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
-      -DTARGET_POSTFIX= ^
+      -DPA_LIBNAME_ADD_SUFFIX:BOOL=OFF ^
+      -DPA_BUILD_STATIC:BOOL=OFF ^
       .
-cmake --build . --target ALL_BUILD --config Release
-copy /y Release\*.dll %LIBRARY_BIN%
-copy /y Release\*.lib %LIBRARY_LIB%
-copy /y include\*.h %LIBRARY_INC%
+if errorlevel 1 exit 1
+
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+cmake --install . --config Release
+if errorlevel 1 exit 1
+
